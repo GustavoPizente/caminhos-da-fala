1b0bc514c7f70d054a81659e2f1e58571c5f86ed
GustavoPizente <gustavonazarine@gmail.com>
2025-03-29 19:06:08 -0300

    ajuste na margin do body

--- src/app/Components/Hamburguer.tsx
+++ src/app/Components/Hamburguer.tsx
@@ -14,7 +14,7 @@ const Hamburguer = () => {
 <path d="M4.82642 20.1227C4.82642 19.8465 5.05027 19.6227 5.32642 19.6227H31.4481C31.7242 19.6227 31.9481 19.8465 31.9481 20.1227V22.6221C31.9481 22.8983 31.7242 23.1221 31.4481 23.1221H18.3872H5.32642C5.05027 23.1221 4.82642 22.8983 4.82642 22.6221V20.1227Z" fill="url(#paint1_linear_2080_16)" stroke="black"/>
 <rect x="4.82642" y="12.8735" width="27.1217" height="3.49945" rx="0.5" fill="url(#paint2_linear_2080_16)" stroke="black"/>
 <defs>
-<filter id="filter0_d_2080_16" x="0" y="0" width="37.7745" height="29.1216" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<filter id="filter0_d_2080_16" x="0" y="0" width="37.7745" height="29.1216" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
 <feFlood floodOpacity="0" result="BackgroundImageFix"/>
 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
 <feOffset dx="1" dy="1"/>
@@ -24,7 +24,7 @@ const Hamburguer = () => {
 <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2080_16" result="shape"/>
 </filter>
 <linearGradient id="paint0_linear_2080_16" x1="18.928" y1="6.1867" x2="18.928" y2="10.1237" gradientUnits="userSpaceOnUse">
-<stop stop-color="#6DA8B8"/>
+<stop stopColor="#6DA8B8"/>
 <stop offset="1" stop-color="#A4CCD6"/>
 </linearGradient>
 <linearGradient id="paint1_linear_2080_16" x1="18.928" y1="19.1227" x2="18.928" y2="23.6221" gradientUnits="userSpaceOnUse">
