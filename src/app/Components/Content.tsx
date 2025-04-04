0ad04f1a381909ea26ee1446b905ae4bdd327e29
GustavoPizente <gustavonazarine@gmail.com>
2025-04-04 19:07:54 -0300

    ajustes no slider do swiper

--- src/app/Components/Content.tsx
+++ src/app/Components/Content.tsx
@@ -3,10 +3,29 @@ import BtnAgendar from "./BtnAgendar";
 import styles from "./content.module.css";
 import Scene from "./Scene";
 import OndasCaminhos from "./OndasCaminhossvg";
+import Slider from "./Slider";
 
 export default function Content() {
   return (
     <div className={styles.content}>
+      
+
+      <div className={styles.banner2}>
+        <Slider/>
+
+        <div className={styles.txtbanner2}>
+          <h1> Fonoaudiologia Integrativa</h1> <br />{" "}
+          <h2>
+            A comunicação é essencial em todas as fases da vida, mas nem sempre
+            damos a devida atenção ao seu desenvolvimento. No Caminhos da Fala,
+            mostro que a fonoaudiologia vai além de corrigir dificuldades na
+            fala – ela ajuda na respiração, na postura e até na forma como nos
+            expressamos no dia a dia. Cuidar da fala é cuidar do corpo e da
+            conexão com o mundo.
+          </h2>
+        </div>
+        <OndasCaminhos/>
+      </div>
       <div className={styles.banner1}>
         <Scene />
 
@@ -27,23 +46,6 @@ export default function Content() {
           <span className={styles.blureffect}></span>
           <span className={styles.shine}></span>
         </button>
-      </div>
-
-      <div className={styles.banner2}>
-        <img src="imgslider.png" className={styles.imgslider} alt="slider" />
-
-        <div className={styles.txtbanner2}>
-          <h1> Fonoaudiologia Integrativa</h1> <br />{" "}
-          <h2>
-            A comunicação é essencial em todas as fases da vida, mas nem sempre
-            damos a devida atenção ao seu desenvolvimento. No Caminhos da Fala,
-            mostro que a fonoaudiologia vai além de corrigir dificuldades na
-            fala – ela ajuda na respiração, na postura e até na forma como nos
-            expressamos no dia a dia. Cuidar da fala é cuidar do corpo e da
-            conexão com o mundo.
-          </h2>
-        </div>
-        <OndasCaminhos/>
       </div>
 
       <div className={styles.sobre}>
---
+++ src/app/Components/Slider.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import styles from "./content.module.css";
+
+export default function Slider() {
+  return (
+    <Swiper
+      modules={[Navigation, Autoplay]}
+      slidesPerView={2.5} // Mostra slide central + partes dos lados
+      spaceBetween={30}
+      centeredSlides={true}
+      navigation
+      loop={true}
+      autoplay={{ delay: 3000, disableOnInteraction: true }}
+      className={styles.slider}
+    >
+      <SwiperSlide>
+        <img src="/imgsobre.png" alt="Imagem 1" className={styles.slideImg} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/imgbanner1.png" alt="Imagem 2" className={styles.slideImg} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/imgslider.png" alt="Imagem 3" className={styles.slideImg} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/imgbanner1.png" alt="Imagem 2" className={styles.slideImg} />
+      </SwiperSlide>
+    </Swiper>
+  );
+}
